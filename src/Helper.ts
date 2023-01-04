09879f5d726a027ed0b8557f81d6a94b13a9dbf2
deBabbbe <neotbh@gmx.de>
2023-01-04 22:07:37 +0100

    Move to lambdas

--- src/Helper.ts
+++ src/Helper.ts
@@ -1,10 +1,9 @@
-export function GenerateRandomInt(min = 1, max = 10): number {
-  return Math.random() * (max - min) + min;
-}
+export const GenerateRandomInt = (min = 1, max = 10): number =>
+  Math.random() * (max - min) + min;
 
-export function GenerateRandomString(count: number = 10): string {
+export const GenerateRandomString = (count: number = 10): string => {
   const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
   return Array.from(Array(count))
     .map((_) => chars.charAt(Math.floor(Math.random() * chars.length)))
     .join("");
-}
+};
