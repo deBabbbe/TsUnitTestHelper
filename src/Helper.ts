042b14e6e4d490e9b112b08313a70c32fa39779d
deBabbbe <neotbh@gmx.de>
2023-01-04 22:05:23 +0100

    Add refactorings

--- src/Helper.ts
+++ src/Helper.ts
@@ -2,12 +2,9 @@ export function GenerateRandomInt(min = 1, max = 10): number {
   return Math.random() * (max - min) + min;
 }
 
-export function GenerateRandomString(count: number = 10) {
-  let result = "";
-  const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-  const charactersLength = characters.length;
-  for (let i = 0; i < count; i++) {
-    result += characters.charAt(Math.floor(Math.random() * charactersLength));
-  }
-  return result;
+export function GenerateRandomString(count: number = 10): string {
+  const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+  return Array.from(Array(count))
+    .map((_) => chars.charAt(Math.floor(Math.random() * chars.length)))
+    .join("");
 }
